92397b410da4aba144de233cd4ca197bbf7ab443
DenysSidorov <000scorpions000@gmail.com>
2017-11-26 13:39:01 +0200

    init app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import Vue from './node_modules/vue/dist/vue.js'
-
+import Hello from './components/firstVueComponent';
 new Vue({
+    components: {hello: Hello},
     el: "#app",
 })
---
+++ src/components/firstVueComponent.js
@@ -0,0 +1,5 @@
+<template>
+
+<h2>12</h2>
+
+</template>

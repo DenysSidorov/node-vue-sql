c645c67dbf8483024eec9b911e3c649f32ac492e
DenysSidorov <000scorpions000@gmail.com>
2017-11-26 13:58:57 +0200

    add state from main component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Hello from './components/firstVueComponent.vue';
 new Vue({
-    components: {hello: Hello},
     el: "#app",
+    components: {hello: Hello},
+    data: {
+        message: "Hello Vue"
+    }
 })

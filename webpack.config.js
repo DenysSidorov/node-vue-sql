e34198616c07199eef2e4fc173c2d78bc3b63516
DenysSidorov <000scorpions000@gmail.com>
2017-11-27 21:18:03 +0200

    change webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,35 @@
 module.exports = {
+    // This is the "main" file which should include all other modules
+    entry: './src/app.js',
+    // Where should the compiled file go?
+    output: {
+        // To the `dist` folder
+        path: './www',
+        // With the filename `build.js` so it's dist/build.js
+        filename: 'build.js'
+    },
     module: {
-        rules: [
+        // Special compilation rules
+        loaders: [
             {
-                test: /\.vue$/,
-                loader: 'vue-loader'
+                // Ask webpack to check: If this file ends with .js, then apply some transforms
+                test: /\.js$/,
+                // Transform it with babel
+                loader: 'babel',
+                // don't transform node_modules folder (which don't need to be compiled)
+                exclude: /node_modules/
             }
         ]
     }
 }
+
+// module.exports = {
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.vue$/,
+//                 loader: 'vue-loader'
+//             }
+//         ]
+//     }
+// }

a4eb11f2480039d8f7c016b0340e234b2980eeaf
DenysSidorov <000scorpions000@gmail.com>
2017-11-27 22:48:01 +0200

    delete mess

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,21 @@ var  webpack = require('webpack');
 var env = 'development';
 var config =  {
     context: path.resolve(__dirname, './src'),
-    // This is the "main" file which should include all other modules
     entry: './app.js',
-    // Where should the compiled file go?
     output: {
-        // To the `dist` folder
         path: path.resolve(__dirname, './www'),
-        // With the filename `build.js` so it's dist/build.js
         filename: 'build.js',
         publicPath: '/'
     },
 
     module: {
-        // Special compilation rules
         loaders: [
             {test: /\.html$/, use: 'vue-template-loader'},
-            {
-                // Ask webpack to check: If this file ends with .js, then apply some transforms
-                test: /\.js$/,
-                // Transform it with babel
-                loader: 'babel-loader',
-                // don't transform node_modules folder (which don't need to be compiled)
-                exclude: /node_modules/
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            }
+            {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+            {test: /\.vue$/, loader: 'vue-loader'}
         ]
     },
     plugins: [
-
         new webpack.LoaderOptionsPlugin({
             vue: {
                 loaders: {
